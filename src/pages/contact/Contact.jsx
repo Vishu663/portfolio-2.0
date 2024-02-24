6128e399f8c46cf53d767b4aa13c08553bb19d8a
unknown <vishalsanap663@gmail.com>
2024-02-24 16:10:50 +0530

    form enabled

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -3,8 +3,43 @@ import "./Contact.css";
 import Typewriter from "typewriter-effect";
 import { MdEmail } from "react-icons/md";
 import { FaGithub, FaInstagram } from "react-icons/fa";
+import { useState } from "react";
 
 export default function Contact() {
+	const [user,setUser] = useState({
+		name: '', email: '',subject: '',message: ''
+	})
+	let name,value
+	const data = (e) => {
+		name = e.target.name;
+		value = e.target.value;
+		setUser({...user, [name]: value});
+		console.log(user);
+	}
+	const getData = (e) => {
+		const {name, email, subject, message} = user;
+		e.preventDefault();
+		const options = {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				name, email, subject, message
+			})
+		}
+		const res = fetch(
+			'https://portfolio-1aa30-default-rtdb.asia-southeast1.firebasedatabase.app/UserData.json',
+			options
+		)
+		if(res) {
+			alert('Message Sent')
+		}
+		else {
+			alert('error sending message')
+		}
+
+	}
 	return (
 		<>
 			<Element name="contact">
@@ -45,12 +80,12 @@ export default function Contact() {
 						</div>
 					</div>
 					<div className="contact-right">
-						<form className="contact-form">
-							<input type="name" placeholder="Enter your full name" />
-							<input type="email" placeholder="Enter your email" />
-							<input type="text" placeholder="Enter the Subject"/>
-							<input type="text" placeholder="Type your Message Here"/>
-                            <button>Submit</button>
+						<form className="contact-form" method='POST'>
+							<input type="name" name="name" value={user.name} placeholder="Enter your full name" required onChange={data} />
+							<input type="email" name="email" value={user.email} placeholder="Enter your email" required onChange={data} />
+							<input type="text" name="subject" value={user.subject} placeholder="Enter the Subject" required onChange={data} />
+							<textarea type="text" name="message" value={user.message} placeholder="Type your Message Here" required onChange={data} />
+                            <button onClick={getData}>Submit</button>
 						</form>
 					</div>
 				</div>
