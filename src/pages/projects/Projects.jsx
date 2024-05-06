66e6ce385dbf543079fac995718599109b3d8103
vishal <vishalsanap663@gmail.com>
2024-05-06 12:37:58 +0530

    responsivness achieved

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -4,15 +4,18 @@ import wp from "../../assets/webprint-current.png";
 import ac from "../../assets/Airbnb-clone-ipad.png";
 
 export default function Projects() {
-	return (
-		<>
-			<Element name="projects">
-				<div className="projects-main">
-					<div className="project-card"><img className="card-img" src={wp} alt="" /></div>
-					<div className="project-card"><img className="card-img" src={ac} alt="" /></div>
-					<div className="project-card"></div>
-				</div>
-			</Element>
-		</>
-	);
+  return (
+    <>
+      <Element name="projects">
+        <div className="projects-main">
+          <div className="project-card">
+            <img className="card-img" src={wp} alt="" />
+          </div>
+          <div className="project-card">
+            <img className="card-img" src={ac} alt="" />
+          </div>
+        </div>
+      </Element>
+    </>
+  );
 }
