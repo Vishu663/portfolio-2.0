c722f14e8192243d6598a362cea2c4b601ab7499
vishal <vishalsanap663@gmail.com>
2024-05-09 15:28:13 +0530

    deploy commit

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,51 +5,53 @@ import { FaLinkedin, FaGithub, FaFileAlt } from "react-icons/fa";
 import { Element, Link } from "react-scroll";
 
 export default function Home() {
-	return (
-		<>
-			<Element name="home">
-				<div className="home-main">
-					<div className="main-left">
-						<div className="main-tw">
-							<span>Hello I'm</span>
-							<Typewriter
-								options={{
-									strings: [
-										"Vishal",
-										"a Software Developer",
-										" a Frontend Developer",
-									],
-									autoStart: true,
-									loop: true,
-								}}
-							/>
-						</div>
-						<div className="main-socials">
-							<a href="https://github.com/Vishu663" target="_blank">
-								<FaGithub className="social-icon" />
-							</a>
-							<a
-								href="https://www.linkedin.com/in/vishal-sanap-83537119b/"
-								target="_blank"
-							>
-								<FaLinkedin className="social-icon" />
-							</a>
-							<a
-								href="https://www.canva.com/design/DAFwd067WvU/GpA3rnDy-0-FS689Y7P18A/view?utm_content=DAFwd067WvU&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink"
-								target="_blank"
-							>
-								<FaFileAlt className="social-icon" />
-							</a>
-						</div>
-						<div className="main-button">
-						<Link to="contact" smooth={true} offset={-67} duration={500}><button>Get free quote</button></Link>
-						</div>
-					</div>
-					<div className="main-right">
-						<img className="coder1" src={coder} alt="coder" />
-					</div>
-				</div>
-			</Element>
-		</>
-	);
+  return (
+    <>
+      <Element name="home">
+        <div className="home-main">
+          <div className="main-left">
+            <div className="main-tw">
+              <span>Hello I'm</span>
+              <Typewriter
+                options={{
+                  strings: [
+                    "Vishal",
+                    "a Software Developer",
+                    " a Frontend Developer",
+                  ],
+                  autoStart: true,
+                  loop: true,
+                }}
+              />
+            </div>
+            <div className="main-socials">
+              <a href="https://github.com/Vishu663" target="_blank">
+                <FaGithub className="social-icon" />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/vishal-sanap-83537119b/"
+                target="_blank"
+              >
+                <FaLinkedin className="social-icon" />
+              </a>
+              <a
+                href="https://drive.google.com/file/d/1LjLmTrX1pdg6A1fnOrIm5T7fCMLeJwjr/view?usp=sharing"
+                target="_blank"
+              >
+                <FaFileAlt className="social-icon" />
+              </a>
+            </div>
+            <div className="main-button">
+              <Link to="contact" smooth={true} offset={-67} duration={500}>
+                <button>Get free quote</button>
+              </Link>
+            </div>
+          </div>
+          <div className="main-right">
+            <img className="coder1" src={coder} alt="coder" />
+          </div>
+        </div>
+      </Element>
+    </>
+  );
 }
--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -12,11 +12,13 @@ export default function Projects() {
             <div className="project-card">
               <img className="card-img" src={wp} alt="" />
             </div>
+            <h3>WEBPRINT</h3>
           </a>
           <a href="https://github.com/Vishu663/AIRBNB-CLONE">
             <div className="project-card">
               <img className="card-img" src={ac} alt="" />
             </div>
+            <h3>Hotel-Booking-App</h3>
           </a>
         </div>
       </Element>
