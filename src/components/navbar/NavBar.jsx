dd7faad069f008f2d572378d1dac401175b4bd7c
vishal <vishalsanap663@gmail.com>
2024-05-10 12:10:55 +0530

    fixed hamburger menu

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,18 +1,67 @@
 import { Link } from "react-scroll";
-import "./NavBar.css";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { useState } from "react";
+import "./NavBar.css";
 
 export default function NavBar() {
-    return <>
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
     <div className="navbar">
-        <h3>Vishal Sanap</h3>
-        <GiHamburgerMenu className="navbar-icon" />
-        <ul className="nav-list">
-            <li><Link to="home" smooth={true} offset={-73} duration={500}>Home</Link></li>
-            <li><Link to="about" smooth={true} offset={-70} duration={500}>About</Link></li>
-            <li><Link to="projects" smooth={true} offset={-70} duration={500}>Projects</Link></li>
-            <li><Link to="contact" smooth={true} offset={-67} duration={500}>Contact</Link></li>
+      <h3>Vishal Sanap</h3>
+      <div className="menu-logic">
+        <ul className={`nav-list ${isOpen ? "show" : ""}`}>
+          <li>
+            <Link
+              to="home"
+              smooth={true}
+              offset={-73}
+              duration={500}
+              onClick={toggleMenu}
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="about"
+              smooth={true}
+              offset={-70}
+              duration={500}
+              onClick={toggleMenu}
+            >
+              About
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="projects"
+              smooth={true}
+              offset={-70}
+              duration={500}
+              onClick={toggleMenu}
+            >
+              Projects
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="contact"
+              smooth={true}
+              offset={-67}
+              duration={500}
+              onClick={toggleMenu}
+            >
+              Contact
+            </Link>
+          </li>
         </ul>
+        <GiHamburgerMenu className="navbar-icon" onClick={toggleMenu} />
+      </div>
     </div>
-    </>
+  );
 }
