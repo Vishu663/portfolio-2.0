409e5145b31e6f1154f6ce27603ec88032dce7f2
vishal <vishalsanap663@gmail.com>
2024-05-10 12:48:48 +0530

    minor issue

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,17 @@ import Contact from "./pages/contact/Contact";
 import Home from "./pages/home/Home";
 import Projects from "./pages/projects/Projects";
 import "./App.css";
-import ComingSoon from "./pages/comingsoon/ComingSoon";
 
 function App() {
-  const path = window.location.pathname;
-  if (path === "/ComingSoon") {
-    return <ComingSoon />;
-  } else {
-    return (
-      <div>
-        <NavBar />
-        <Home />
-        <About />
-        <Projects />
-        <Contact />
-      </div>
-    );
-  }
+  return (
+    <div>
+      <NavBar />
+      <Home />
+      <About />
+      <Projects />
+      <Contact />
+    </div>
+  );
 }
 
 export default App;
