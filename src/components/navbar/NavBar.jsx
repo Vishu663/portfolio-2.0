989c3af0f15ebe04eb91cb0ad1e3ddf3deef79dc
vishal <vishalsanap663@gmail.com>
2024-05-10 22:48:49 +0530

    fixed navbar

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -16,46 +16,22 @@ export default function NavBar() {
       <div className="menu-logic">
         <ul className={`nav-list ${isOpen ? "show" : ""}`}>
           <li>
-            <Link
-              to="home"
-              smooth={true}
-              offset={-73}
-              duration={500}
-              onClick={toggleMenu}
-            >
+            <Link to="home" smooth={true} offset={-73} duration={500}>
               Home
             </Link>
           </li>
           <li>
-            <Link
-              to="about"
-              smooth={true}
-              offset={-70}
-              duration={500}
-              onClick={toggleMenu}
-            >
+            <Link to="about" smooth={true} offset={-70} duration={500}>
               About
             </Link>
           </li>
           <li>
-            <Link
-              to="projects"
-              smooth={true}
-              offset={-70}
-              duration={500}
-              onClick={toggleMenu}
-            >
+            <Link to="projects" smooth={true} offset={-70} duration={500}>
               Projects
             </Link>
           </li>
           <li>
-            <Link
-              to="contact"
-              smooth={true}
-              offset={-67}
-              duration={500}
-              onClick={toggleMenu}
-            >
+            <Link to="contact" smooth={true} offset={-67} duration={500}>
               Contact
             </Link>
           </li>
